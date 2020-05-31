1dbbd555caf70260ac581404138ba73194c9dcff
ayane-iga <ayane603@outlook.jp>
2020-05-31 16:39:21 +0900

    submit

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
   
   resources :books do
     resource :favorites, only: [:create, :destroy]
-    resources :book_comments, only: [:create]
+    resources :book_comments, only: [:create, :destroy]
   end
 
-  resources :book_comments, only: [:destroy]
+  #resources :book_comments, only: [:destroy]
 
   root 'home#top'
   get 'home/about'
